fa3a30457ce3bbc08792564cb6edb0ce5e923d63
mohamed daahir abdi <muniirm81@gmail.com>
2023-05-16 17:06:58 +0300

    css skyline freecodecamp

---
+++ main.js
@@ -0,0 +1,25 @@
+/* const a = ["muniir", "cali", "qurux","foolxumo"]
+a.forEach(function(l) {
+    console.log(l)
+}) */
+
+// function declarations
+
+/*
+function square (num){
+    return num * num
+}
+console.log(square(5)) */
+
+// function expressions
+
+/*
+const add = function (num){
+    return num + num 
+}
+console.log(add(5)) */ 
+
+// arrow functions
+
+let add = (num) => {return num * num } ;
+console.log(add(6))
